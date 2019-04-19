d737533d7dd7d7ee79391a8988ccf420bbf03d11
florian lahitte <lahitte.florian@gmail.com>
2019-04-19 14:56:49 -0400

    clean code

--- db/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE every_mundo;
-USE every_mundo;
-
-CREATE TABLE flight (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Bacon Cheese', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Tomato Chesse', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burger Pickles Cheese', false);
