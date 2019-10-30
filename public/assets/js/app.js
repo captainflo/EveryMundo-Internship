b35e8e5395fdf28fa617481ca51f53ab8e881075
florian lahitte <lahitte.florian@gmail.com>
2019-10-30 10:43:57 -0400

    add port

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,21 +1,22 @@
 // Disable form return for One way
-    $(document).on("click","#exampleRadios2",function() {
-        $('.return').prop('disabled', true);
-    });
-    $(document).on("click","#exampleRadios1",function() {
-        $('.return').prop('disabled', false);
-    });
-const dataArray = []; 
+$(document).on('click', '#exampleRadios2', function() {
+  $('.return').prop('disabled', true);
+});
+$(document).on('click', '#exampleRadios1', function() {
+  $('.return').prop('disabled', false);
+});
+const dataArray = [];
 // Get all data from https://everymundointernship.herokuapp.com/popularRoutes/
-    function infoFlight(){
-        $(".showFlight").empty();
-        var queryURL = 'https://everymundointernship.herokuapp.com/popularRoutes/WC95RP59BM57';
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(function(data){
-            for(i=0; i < data.length; i++){
-              $(".showFlight").append(`
+function infoFlight() {
+  $('.showFlight').empty();
+  var queryURL =
+    'https://everymundointernship.herokuapp.com/popularRoutes/WC95RP59BM57';
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(data) {
+    for (i = 0; i < data.length; i++) {
+      $('.showFlight').append(`
               <div class="col-xs-12 col-md-4">
                 <div class="card">
                     <img class="card-img-top" src="${data[i].routeCoverImage}" alt="Card image cap">
@@ -31,44 +32,45 @@ const dataArray = [];
                     <a href="#" data-id="${i}" class="button-by-card float-none btn btn-primary">View Detail</a>    
                 </div>
               </div>
-              `); 
-                    
-            }
-        });
+              `);
     }
-    infoFlight();
+  });
+}
+infoFlight();
 
-    // info By card 
-    function infoBycard(){
-        $(document).on("click",".button-by-card",function(event) {
-        event.preventDefault(); 
-        queryUrl = 'https://cors-anywhere.herokuapp.com/';
-        queryUrl += 'https://everymundointernship.herokuapp.com/search/WC95RP59BM57';
-        
-        var thisId = $(this).attr("data-id");
+// info By card
+function infoBycard() {
+  $(document).on('click', '.button-by-card', function(event) {
+    event.preventDefault();
+    queryUrl = 'https://cors-anywhere.herokuapp.com/';
+    queryUrl +=
+      'https://everymundointernship.herokuapp.com/search/WC95RP59BM57';
 
-        flight = {
-            destination: $('.des-' + thisId).text(),
-            origin: $('.ori-' + thisId).text(),
-            tripType: $('.type-' + thisId).text(),
-            departureDate:  $('.date-' + thisId).text(),
-            returnDate: "",
-            passengerCount: 1
-        };
+    var thisId = $(this).attr('data-id');
 
-        console.log(flight);
-        $.ajax({
-            url: queryUrl,
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify( flight ),
-            dataType: 'json'
-        }).done( data => {
-            console.log(data);
-            for(i=0; i < data.length; i++){
-                dataArray.push(data[i]);
-                $(".showFlight").empty();
-                $(".showFlight").append(`
+    flight = {
+      destination: $('.des-' + thisId).text(),
+      origin: $('.ori-' + thisId).text(),
+      tripType: $('.type-' + thisId).text(),
+      departureDate: $('.date-' + thisId).text(),
+      returnDate: '',
+      passengerCount: 1
+    };
+
+    console.log(flight);
+    $.ajax({
+      url: queryUrl,
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(flight),
+      dataType: 'json'
+    })
+      .done(data => {
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+          dataArray.push(data[i]);
+          $('.showFlight').empty();
+          $('.showFlight').append(`
                 <div class="col-xs-12 col-md-12">
                     <div class="card">
                         <img class="card-img" src="http://usatravelguru.com/wp-content/uploads/2016/05/usatravel-slidera3.jpg" alt="Card image cap">
@@ -94,61 +96,70 @@ const dataArray = [];
                 </div>
                 `);
 
-                // const priceSort = _.orderBy(data[i].routes, ['priceUSD'],['asc']);
-                // const timeSort = _.orderBy(data[i].routes, ['departureTime'],['asc']);
-                
-                for (let j = 0; j < data[i].routes.length; j++) {
-                    $("#"+i).append(`
+          // const priceSort = _.orderBy(data[i].routes, ['priceUSD'],['asc']);
+          // const timeSort = _.orderBy(data[i].routes, ['departureTime'],['asc']);
+
+          for (let j = 0; j < data[i].routes.length; j++) {
+            $('#' + i).append(`
                     <tr>
                         <td>${data[i].routes[j].departureTime}</td>
                         <td>${data[i].routes[j].arrivalTime}</td>
                         <td id="price">$${data[i].routes[j].priceUSD}</td>
                     </tr>
-                    `)
-                } 
-            }
-        }).fail( err => {
-            console.error( err );
-            alert("There is no flight");
-        });
-        });
-    }
-    infoBycard();
-    
-    // Search Form 
-    function infosearch(){
-        $(document).on("click",".flight-button",function(event) {
-            event.preventDefault();
-            $(".showFlight").empty();
+                    `);
+          }
+        }
+      })
+      .fail(err => {
+        console.error(err);
+        alert('There is no flight');
+      });
+  });
+}
+infoBycard();
 
-            queryUrl = 'https://cors-anywhere.herokuapp.com/';
-            queryUrl += 'https://everymundointernship.herokuapp.com/search/WC95RP59BM57';
-            
-            var departureDate = $(".depart").val()
-            var returnDate = $(".return").val()
-            returnDate = moment(returnDate).format("L") || "";
+// Search Form
+function infosearch() {
+  $(document).on('click', '.flight-button', function(event) {
+    event.preventDefault();
+    $('.showFlight').empty();
 
-            flight = {
-               destination: $('.to').val().trim(),
-               origin: $('.from').val().trim(),
-               tripType: $('input[name=exampleRadios]:checked').val(),
-               departureDate:  moment(departureDate).format("L"),
-               returnDate: returnDate,
-               passengerCount: $('#exampleFormControlSelect1').find(":selected").index()
-            };
+    queryUrl = 'https://cors-anywhere.herokuapp.com/';
+    queryUrl +=
+      'https://everymundointernship.herokuapp.com/search/WC95RP59BM57';
 
-            console.log(flight);
-            $.ajax({
-             url: queryUrl,
-             method: 'POST',
-             contentType: 'application/json',
-             data: JSON.stringify( flight ),
-             dataType: 'json'
-            }).done( data => {
-                for(i=0; i < data.length; i++){
-                    dataArray.push(data[i]);
-                    console.log(dataArray);
-                    $(".showFlight").append(`
+    var departureDate = $('.depart').val();
+    var returnDate = $('.return').val();
+    returnDate = moment(returnDate).format('L') || '';
+
+    flight = {
+      destination: $('.to')
+        .val()
+        .trim(),
+      origin: $('.from')
+        .val()
+        .trim(),
+      tripType: $('input[name=exampleRadios]:checked').val(),
+      departureDate: moment(departureDate).format('L'),
+      returnDate: returnDate,
+      passengerCount: $('#exampleFormControlSelect1')
+        .find(':selected')
+        .index()
+    };
+
+    console.log(flight);
+    $.ajax({
+      url: queryUrl,
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(flight),
+      dataType: 'json'
+    })
+      .done(data => {
+        for (i = 0; i < data.length; i++) {
+          dataArray.push(data[i]);
+          console.log(dataArray);
+          $('.showFlight').append(`
                     <div class="col-xs-12 col-md-12">
                         <div class="card">
                             <img class="card-img" src="http://usatravelguru.com/wp-content/uploads/2016/05/usatravel-slidera3.jpg" alt="Card image cap">
@@ -173,63 +184,64 @@ const dataArray = [];
                         </div>
                     </div>
                     `);
-                    // const priceSort = _.orderBy(data[i].routes, ['priceUSD'],['asc']);
-                    // const timeSort = _.orderBy(data[i].routes, ['departureTime'],['asc']); 
-                    
-                    for (let j = 0; j < data[i].routes.length; j++) {
-                        $("#"+i).append(`
+          // const priceSort = _.orderBy(data[i].routes, ['priceUSD'],['asc']);
+          // const timeSort = _.orderBy(data[i].routes, ['departureTime'],['asc']);
+
+          for (let j = 0; j < data[i].routes.length; j++) {
+            $('#' + i).append(`
                         <tr>
                             <td>${data[i].routes[j].departureTime}</td>
                             <td>${data[i].routes[j].arrivalTime}</td>
                             <td id="price">$${data[i].routes[j].priceUSD}</td>
                         </tr>
-                        `)
-                    } 
-                }
-           }).fail( err => {
-               console.error( err );
-               alert("There is no flight");
-           });
-        });
-    }
-    infosearch();
+                        `);
+          }
+        }
+      })
+      .fail(err => {
+        console.error(err);
+        alert('There is no flight');
+      });
+  });
+}
+infosearch();
 
-    // Click by Score.
-       $(document).on("click",".pricesort",function() {
-        var thisID = $(this).attr("data-price");
-        $("#"+thisID).empty();
+// Click by Score.
+$(document).on('click', '.pricesort', function() {
+  var thisID = $(this).attr('data-price');
+  $('#' + thisID).empty();
 
-        const priceSort = _.orderBy(dataArray[thisID].routes, ['priceUSD'],['asc']);
+  const priceSort = _.orderBy(dataArray[thisID].routes, ['priceUSD'], ['asc']);
 
-        for (let j = 0; j < priceSort.length; j++) {
-            $("#"+thisID).append(`
+  for (let j = 0; j < priceSort.length; j++) {
+    $('#' + thisID).append(`
             <tr>
                 <td>${priceSort[j].departureTime}</td>
                 <td>${priceSort[j].arrivalTime}</td>
                 <td id="price">$${priceSort[j].priceUSD}</td>
             </tr>
-            `)
-        } 
-    });
+            `);
+  }
+});
 
-        // Click by Time.
-        $(document).on("click",".timesort",function() {
-            var thisID = $(this).attr("data-time");
-            $("#"+thisID).empty();
+// Click by Time.
+$(document).on('click', '.timesort', function() {
+  var thisID = $(this).attr('data-time');
+  $('#' + thisID).empty();
 
-            const timeSort = _.orderBy(dataArray[thisID].routes, ['departureTime'],['desc']); 
-            console.log(timeSort);
-            for (let j = 0; j < timeSort.length; j++) {
-                $("#"+thisID).append(`
+  const timeSort = _.orderBy(
+    dataArray[thisID].routes,
+    ['departureTime'],
+    ['desc']
+  );
+  console.log(timeSort);
+  for (let j = 0; j < timeSort.length; j++) {
+    $('#' + thisID).append(`
                 <tr>
                     <td>${timeSort[j].departureTime}</td>
                     <td>${timeSort[j].arrivalTime}</td>
                     <td id="price">$${timeSort[j].priceUSD}</td>
                 </tr>
-                `)
-            } 
-        });
-
-    
-
-    
+                `);
+  }
+});
